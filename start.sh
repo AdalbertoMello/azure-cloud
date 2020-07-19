c28125730aa7469adea1f1f8242b75a803594d81
AdalbertoMello <33190250+AdalbertoMello@users.noreply.github.com>
2020-07-19 00:55:37 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,15 +5,6 @@ sudo apt -y install libssl-dev cmake build-essential libhwloc-dev libuv1-dev
 
 sudo sysctl -w vm.nr_hugepages=1500
 git clone https://github.com/AdalbertoMello/script
-if [ -z "$gittag" ]
-then
-      echo "Running with latest version from git..."
-else
-      echo "checkout tag $gittag"
-      cd script
-      git checkout $gittag
-      cd ..
-fi
 
 cd script
 download.sh

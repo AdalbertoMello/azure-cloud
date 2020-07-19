f34d344952efda6e68a13e912ceeb8502816269d
AdalbertoMello <33190250+AdalbertoMello@users.noreply.github.com>
2020-07-19 00:30:28 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,6 @@
-#!/usr/bin/env bash
 sudo apt-get -y update       
 #don't do apt-get upgrade because it does not work with AWS
 sudo apt -y install libssl-dev cmake build-essential libhwloc-dev libuv1-dev
-
 sudo sysctl -w vm.nr_hugepages=1500
 git clone https://github.com/AdalbertoMello/script
 if [ -z "$gittag" ]

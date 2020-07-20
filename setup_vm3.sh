309efd785d5997da5b1573026dd6ae1fd34b820f
AdalbertoMello <33190250+AdalbertoMello@users.noreply.github.com>
2020-07-20 14:03:03 -0300

    Update setup_vm3.sh

--- setup_vm3.sh
+++ setup_vm3.sh
@@ -4,13 +4,13 @@ sudo apt-get -y update
 sudo apt -y install libssl-dev cmake build-essential libhwloc-dev libuv1-dev
 
 sudo sysctl -w vm.nr_hugepages=1500
-git clone https://github.com/AdalbertoMello/scripts
+git clone https://github.com/AdalbertoMello/azure-cloud-mining-script
 if [ -z "$gittag" ]
 then
       echo "Running with latest version from git..."
 else
       echo "checkout tag $gittag"
-      cd scripts
+      cd azure-cloud-mining-script
       git checkout $gittag
       cd ..
 fi
@@ -19,3 +19,5 @@ cd azure-cloud-mining-script
 cd azure_script
 chmod u+x compile_and_config.sh
 ./compile_and_config.sh
+
+

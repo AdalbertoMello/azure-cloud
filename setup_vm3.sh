fa00321e4661235959bc316ac7712e0d0ef80a65
AdalbertoMello <33190250+AdalbertoMello@users.noreply.github.com>
2020-07-19 22:26:13 -0300

    Create setup_vm3.sh

---
+++ setup_vm3.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+sudo apt-get -y update       
+#don't do apt-get upgrade because it does not work with AWS
+sudo apt -y install libssl-dev cmake build-essential libhwloc-dev libuv1-dev
+
+sudo sysctl -w vm.nr_hugepages=1500
+git clone https://github.com/AdalbertoMello/scripts
+if [ -z "$gittag" ]
+then
+      echo "Running with latest version from git..."
+else
+      echo "checkout tag $gittag"
+      cd scripts
+      git checkout $gittag
+      cd ..
+fi
+
+cd scripts/azure
+chmod u+x compile_and_config.sh
+./compile_and_config.sh

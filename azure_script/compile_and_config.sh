f53a50133d9bd14c8a3920e157ce5db9fcc83aa1
U117A <33190250+U117A@users.noreply.github.com>
2020-07-22 23:08:43 -0300

    Update compile_and_config.sh

--- azure_script/compile_and_config.sh
+++ azure_script/compile_and_config.sh
@@ -6,10 +6,8 @@ rm -rf xmrig/
 git clone https://github.com/xmrig/xmrig.git
 cd xmrig
 git checkout v5.5.3
-cd src
 sed -i 's/kDefaultDonateLevel =.*/kDefaultDonateLevel = 0;/' src/donate.h
 sed -i 's/kMinimumDonateLevel =.*/kMinimumDonateLevel = 0;/' src/donate.h
-cd ..
 mkdir build
 cd build
 cmake ..

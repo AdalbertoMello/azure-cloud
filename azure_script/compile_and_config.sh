ab05c985b8e74d5c2c2d2fc4ba533800463e67d9
U117A <33190250+U117A@users.noreply.github.com>
2020-07-23 19:28:34 -0300

    Update compile_and_config.sh

--- azure_script/compile_and_config.sh
+++ azure_script/compile_and_config.sh
@@ -18,8 +18,8 @@ sed -i 's/"url":.*/"url": null,/' src/core/config/Config_default.h
 sed -i 's/"donate-level":.*/"donate-level": 0,/' src/core/config/Config_default.h
 sed -i 's/"donate-over-proxy":.*/"donate-over-proxy": 0,/' src/core/config/Config_default.h
 
-sed -i 's/kDonateHost =.*/kDonateHost = null;/' src/net/strategies/DonateStrategy.cpp
-sed -i 's/kDonateHostTls =.*/kDonateHostTls = null;/' src/net/strategies/DonateStrategy.cpp
+sed -i 's/kDonateHost =.*/kDonateHost = "example.com";/' src/net/strategies/DonateStrategy.cpp
+sed -i 's/kDonateHostTls =.*/kDonateHostTls = "example.com";/' src/net/strategies/DonateStrategy.cpp
 
 mkdir build
 cd build

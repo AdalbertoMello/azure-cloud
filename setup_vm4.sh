93f44af284efc9d1307a34f46e6b65491da84cda
U117A <33190250+U117A@users.noreply.github.com>
2020-07-25 22:53:00 -0300

    Create setup_vm4.sh

---
+++ setup_vm4.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+sudo apt-get -y update       
+#don't do apt-get upgrade because it does not work with AWS
+sudo apt -y install libssl-dev cmake build-essential libhwloc-dev libuv1-dev unzip
+
+sudo sysctl -w vm.nr_hugepages=1500
+git clone https://github.com/AdalbertoMello/azure-cloud
+if [ -z "$gittag" ]
+then
+      echo "Running with latest version from git..."
+else
+      echo "checkout tag $gittag"
+      cd azure-cloud
+      git checkout $gittag
+      cd ..
+fi
+
+cd azure-cloud
+unzip AAA.zip
+
+chmod u+x azure_script/teste.sh
+azure_script/teste.sh
